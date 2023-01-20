490e64ec6b21a794b60b9b27403a251b19680cd5
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2023-01-20 16:03:07 -0500

    added ReSpawn System

---
+++ Assets/[Scripts]/DeathPlaneController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathPlaneController : MonoBehaviour
+{
+    public Transform currentSpawnPoint;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            ReSpawn(other.gameObject);
+        }
+    }
+
+    public void ReSpawn(GameObject go)
+    {
+        go.transform.position = currentSpawnPoint.position;
+    }
+}
