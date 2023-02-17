647b373baacba2cec731ae4a62fcc985b8685ae3
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2023-02-17 15:23:12 -0500

    added EnemyController v2

--- Assets/[Scripts]/EnemyController.cs
+++ Assets/[Scripts]/EnemyController.cs
@@ -14,18 +14,44 @@ public class EnemyController : MonoBehaviour
     public bool isObstacleInFront;
     public bool isGroundAhead;
     public bool isGrounded;
+    public Vector2 direction;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        direction = Vector2.left;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        isObstacleInFront = Physics2D.Linecast(groundPoint.position, inFrontCheck.position, groundLayerMask);
+        isGroundAhead = Physics2D.Linecast(groundPoint.position, groundAheadCheck.position, groundLayerMask);
+        isGrounded = Physics2D.OverlapCircle(groundPoint.position, groundRadius, groundLayerMask);
+
+        if (isGroundAhead)
+        {
+            Move();
+        }
+
+        if (isObstacleInFront || !isGroundAhead)
+        {
+            Flip();
+        }
     }
+
+    void Move()
+    {
+        transform.position += new Vector3(direction.x * horizontalSpeed * Time.deltaTime, 0.0f);
+    }
+
+    void Flip()
+    {
+        var x = transform.localScale.x * -1.0f;
+        direction *= -1.0f;
+        transform.localScale = new Vector3(x, 1.0f, 1.0f);
+    }
+
 
     public void OnDrawGizmos()
     {
